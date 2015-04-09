8a36d1279bad94180c23c505aa2f140872e35165
jsharin <joel.a.sharin@gmail.com>
2015-04-08 22:35:54 -0400

    Added class for the main game.

---
+++ app/src/main/java/com/joelsharin/brianesch/hexed/com/joelsharin/brianesch/hexed/game/HexGame.java
@@ -0,0 +1,12 @@
+package com.joelsharin.brianesch.hexed.com.joelsharin.brianesch.hexed.game;
+
+/**
+ * Created by Joel on 4/8/2015.
+ * Class contains the information for one game.
+ */
+public class HexGame {
+    private int size;
+    public HexGame(int size) {
+        this.size = size;
+    }
+}
